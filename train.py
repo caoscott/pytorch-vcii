9e3cdc9f23836c5160fd648479c602859f515a32
caoscott <scottcao@outlook.com>
2019-11-07 18:10:18 -0600

    Printing codes

--- train.py
+++ train.py
@@ -151,6 +151,7 @@ def train_loop(batch, crops, ctx_frames):
 
         # Binarize.
         codes = binarizer(encoded)
+        print(codes)
 
         # Decode.
         (output, decoder_h_1, decoder_h_2, decoder_h_3, decoder_h_4) = decoder(
